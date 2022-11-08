c1754e21c34a1975b8a893d5796ba5166a7c484b
davy <dawei.si@qq.com>
2022-11-08 23:57:29 +0800

    Use pass_context pass vault

--- obtool/cli.py
+++ obtool/cli.py
@@ -1,25 +1,30 @@
+from typing import Optional
+
 import click
 
 from obtool.obsidian import get_vaults_list, ObVault
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--vault', default=None, required=False)
+@click.pass_context
+def cli(ctx, vault):
+    if vault:
+        ctx.obj = ObVault.open(vault)
 
 
 @click.command()
-@click.argument('vault', default=None, required=False)
-def ls(vault):
+@click.pass_obj
+def ls(vault: Optional[ObVault]):
     if vault is None:
         for v in get_vaults_list():
             vault_name = v.path.name
             vault_path = v.path.parent
-            click.echo(f'{vault_name}\n    {vault_path}')
+            click.secho(f'{vault_name}', fg='cyan')
+            click.secho(f'    {vault_path}')
     else:
-        ob_vault = ObVault.open(vault)
-        for f in ob_vault.iter_notes():
-            click.echo(f'{f}')
+        for f in vault.iter_notes():
+            click.secho(f'{f.name}', fg='cyan')
 
 
 cli.add_command(ls)
